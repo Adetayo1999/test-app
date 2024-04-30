39704759822815b8cd828b2ea3ecd1877d904d29
Tola-byte <adetolaisaiah17@gmail.com>
2024-04-30 18:18:00 +0100

    feat: services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,10 @@
+import { HeroSection } from "@/common/components/skeleton/hero/Hero";
+import { ServicesHero } from "@/common/components/skeleton/heroservices";
+
+export default function Services() {
+    return (
+        <div className=''>
+            <ServicesHero />
+        </div>
+    );
+}
--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -4,16 +4,13 @@ import services from "../assets/images/services.png"
 import aiinitiative from "../assets/images/aiinitiative.png"
 import bookconsultation from "../assets/images/bookconsultation.png"
 import faqs from "../assets/images/faq.png"
-import { SlideShow, SlideTypes } from "@/common/types/types"
-import image0 from "../assets/images/image0.png"
+import { SlideShow } from "@/common/types/types"
 import image1 from "../assets/images/image1.png"
 import image2 from "../assets/images/image2.png"
 import image3 from "../assets/images/image3.png"
 import image4 from "../assets/images/image4.png"
 import image5 from "../assets/images/image5.png"
-import image6 from "../assets/images/image6.png"
 export const SlideImages:SlideShow[] =[ 
-   
     {
         id: 0,
         image: image1
@@ -34,47 +31,97 @@ export const SlideImages:SlideShow[] =[
         id: 4,
         image: image5
     },
-    {
-        id: 5,
-        image: image6
-    }
-
 ]
 export const HomeImages = [
     {
         id: 1,
         title: "About",
         icon: about,
-        path: ""
+        path: "/about"
     },
     {
         id: 2,
         title: "Design Portfolio",
         icon: designportfolio,
-        path: ""
+        path: "/portfolio"
     },
     {
         id: 3,
         title: "Services",
         icon: services,
-        path: ""
+        path: "/services"
     },
     {
         id: 4,
         title: "AI Initiative",
         icon: aiinitiative,
-        path: ""
+        path: "/ai-initiative"
     },
     {
         id: 5,
         title: "Book a consultation",
         icon: bookconsultation,
-        path: ""
+        path: "/consultation"
     },
     {
         id: 6,
         title: "FAQs",
         icon: faqs,
-        path: ""
+        path: "/faqs"
+    }
+]
+
+export const listOfServices = [
+    {
+        id: 1,
+        title: "Full Service Interior Design for Residential & Boutique"
+    },
+    {
+        id: 2,
+        title: "Commercial Spaces"
+    },
+    {
+        id: 3,
+        title: "Space Planning"
+    },
+    {
+        id: 4,
+        title: "Paint and Wall Covering Consultation"
+    },
+    {
+        id: 5,
+        title: "Kitchen & Bath Design"
+    },
+    {
+        id: 6,
+        title: "Custom Furniture Design & Production"
+    },
+    {
+        id: 7,
+        title: "Custom Cabinetry Design"
+    },
+    {
+        id: 8,
+        title: "Custom Rug Design"
+    },
+    {
+        id: 9,
+        title: "Construction Documents"
+    },
+    {
+        id: 10,
+        title: "Construction Administration"
+    },
+    {
+        id: 11,
+        title: "Furniture, Finish & Fixture Selections & Specifications"
+    },
+    {
+        id: 12,
+        title: "Product Purchasing, Procurement & Installation"
+    },
+    {
+        id: 13,
+        title: "Arts & Accessory Selection, Framing & Placement"
     }
 ]
