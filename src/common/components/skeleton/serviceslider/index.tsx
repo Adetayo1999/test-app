1f935fcff9150cbdb4fa8a98a7aa1c0135b83122
Tola-byte <adetolaisaiah17@gmail.com>
2024-05-02 10:09:44 +0100

    fix: used marquee on services page

--- src/common/components/skeleton/serviceslider/index.tsx
+++ src/common/components/skeleton/serviceslider/index.tsx
@@ -1,57 +1,22 @@
 "use client"
-import Slider from "react-slick";
 import "@/assets/styles/globals.css";
 import Image from "next/image";
 import { SlideTypes } from "@/common/types/slider.types";
+import Marquee from "react-fast-marquee";
 
 export const SliderComponent:React.FC<SlideTypes> = ({listOfSlides}) => {
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        loop: true,
-        speed: 4000,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplay: true,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
+   
     return (
 
-            <div className="-mt-20 bg-[black] pt-10 gap-4">
-                <Slider {...settings}>
+            <div className="-mt-20 bg-[black] pt-10">
+            <Marquee autoFill pauseOnHover>
                 {listOfSlides?.map(item => (
                     <div key={item.id} >
-                        <Image src={item.image} className="rounded-xl w-[370px]" alt="icon" />
+                        <Image src={item.image} className="rounded-xl mr-2 w-[370px]" alt="icon" />
                         </div>
                     ))}
-                </Slider>
+                </Marquee>
         </div>
     )
 }
