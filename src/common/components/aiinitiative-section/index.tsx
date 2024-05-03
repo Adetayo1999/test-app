e660ec3bbaa1638512a7d7584bff8ed6c66943e4
Tola-byte <adetolaisaiah17@gmail.com>
2024-05-03 21:11:15 +0100

    fix: pr-review

--- src/common/components/aiinitiative-section/index.tsx
+++ src/common/components/aiinitiative-section/index.tsx
@@ -1,22 +1,35 @@
-import { SectionTypes } from "@/common/types/section.types"
-import Image from "next/image"
+import { SectionTypes } from "@/common/types/aiinitiativesection.types";
+import Image from "next/image";
 
-export const SectionGenerator: React.FC<SectionTypes>= ({image,subheader,header, textColor,body,reversed}) => {
+export const SectionGenerator: React.FC<SectionTypes> = ({
+    image,
+    subheader,
+    header,
+    textColor,
+    body,
+    reversed,
+}) => {
     return (
-        <div className={`flex flex-col gap-8  ${reversed ? "md:flex-row-reverse md:gap-40" : "md:flex-row"}`}>
-            
-            <div className={`w-full md:w-1/2 ${textColor ? `text-white -mt-4` : `text-[#000] md:mt-36`}`}>
-                <p className="text-[16px] md:text-2xl font-bodoni px-1">{subheader}</p>
-                <p className={`text-xl md:text-6xl font-bold w-full ${textColor ? `md:w-[50vw]` : `md:w-4/5`} mt-10`}>{header}</p>
+        <div
+            className={`flex flex-col gap-8  ${reversed ? "md:flex-row-reverse md:gap-40" : "md:flex-row"}`}>
+            <div
+                className={`w-full md:w-1/2 ${textColor ? `text-white -mt-4` : `text-[#000] md:mt-36`}`}>
+                <p className='text-[16px] md:text-2xl font-bodoni px-1'>
+                    {subheader}
+                </p>
+                <p
+                    className={`text-xl md:text-6xl font-bold w-full ${textColor ? `md:w-[50vw]` : `md:w-4/5`} mt-10`}>
+                    {header}
+                </p>
 
-                <p className="text-[14px] md:text-xl mt-6 md:mt-14 w-full md:w-[90%]">{body}</p>
-
+                <p className='text-[14px] md:text-xl mt-6 md:mt-14 w-full md:w-[90%]'>
+                    {body}
+                </p>
             </div>
 
-            <div className="">
-                <Image src={image} alt="images" />
+            <div className=''>
+                <Image src={image} alt='images' />
             </div>
-
         </div>
-    )
-}
+    );
+};
